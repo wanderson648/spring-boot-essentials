87eb7b0e644cff19a09b78a7cb28bd5990fb7dd2
wanderson648 <testepedroca@gmail.com>
2023-11-01 14:50:23 -0300

    implementa recurso POST

--- src/main/java/com/wo/springboot2/controller/AnimeController.java
+++ src/main/java/com/wo/springboot2/controller/AnimeController.java
@@ -5,11 +5,9 @@ import com.wo.springboot2.service.AnimeService;
 import com.wo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -35,5 +33,12 @@ public class AnimeController {
         log.info("[start] AnimeController - findById");
         log.info("[finished] AnimeController - findById");
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        log.info("[start] AnimeController - save");
+        log.info("[finished] AnimeController - save");
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 }
