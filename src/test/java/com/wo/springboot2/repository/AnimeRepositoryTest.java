6456e3b67f226e68fe64973a6b111ca5966a32eb
wanderson648 <testepedroca@gmail.com>
2023-11-03 14:40:15 -0300

    implementa Test para Spring Data Jpa pt-1

---
+++ src/test/java/com/wo/springboot2/repository/AnimeRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.wo.springboot2.repository;
+
+import com.wo.springboot2.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Tests for anime Repository")
+class AnimeRepositoryTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+    @Test
+    @DisplayName("Save creates anime when Successful")
+    void save_PersistAnime_WhenSuccessful() {
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+    }
+
+    private Anime createAnime() {
+        return Anime.builder()
+                .name("Hajime no Ippo")
+                .build();
+    }
+}
